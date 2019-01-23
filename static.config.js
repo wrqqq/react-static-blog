2bd946f4f72c9ecdeb9a08883a45e2381320c6f4
Илья Солдатенков <ISoldatenkov@WEBBIN.LOCAL>
2019-01-23 15:48:48 +0300

    Fix

--- static.config.js
+++ static.config.js
@@ -11,7 +11,7 @@ export default {
         )
         return [
             {
-                path: '.',
+                path: '/',
                 getData: () => ({
                     posts,
                 }),
