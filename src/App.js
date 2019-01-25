3cf40cf37173dfd0664c31febf16a9a8746deada
Илья Солдатенков <ISoldatenkov@WEBBIN.LOCAL>
2019-01-25 19:15:12 +0300

    Styles, config

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { Root, Routes, Route } from 'react-static'
-import { Link, Location } from '@reach/router'
-import { Transition, animated } from 'react-spring'
+import { Root, Routes } from 'react-static'
 
 
 const App = () => (
-    <Root>
-        <Routes>
-            {({ getComponentForPath }) => {
-                // The pathname is used to retrieve the component for that path
-                let Comp = getComponentForPath(window.location.href)
-                // The component is rendered!
-                return <Comp />
-            }}
-        </Routes>
-    </Root>
+  <Root>
+    <Routes />
+  </Root>
 )
 
 export default App
--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { withRouteData } from 'react-static'
 import { Link } from '@reach/router'
+import styled from 'styled-components'
+
+const Container = styled.div`
+   width: 70%;
+   background:ghostwhite;
+   font-size: 25px;
+   margin: auto;
+   padding: 20px;
+`
 
 export default withRouteData(({ post }) => (
-  <div>
-      {console.log(post)}
-    <Link to="/">{'<'} Back</Link>
-    <br />
-    <h3>{post.data.title}</h3>
-    <p>{post.data.body}</p>
-  </div>
+    <div>
+        <Container>
+            <Link to="/">К списку</Link>
+            {console.log(post)}
+            <br />
+            <h3>{post.data.title}</h3>
+            <p>{post.data.body}</p>
+        </Container>
+    </div>
 ))
